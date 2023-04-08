9dd5777e717fab7636758ed3f8b1cc7872f43b32
Maximiliano Santana <iimaxsany@gmail.com>
2023-04-08 11:08:05 -0600

    Import the first 3D Model maded by me

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 import * as THREE from 'three';
+import { Light } from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -10,6 +12,9 @@ document.body.appendChild(renderer.domElement);
 
 //Geometrys
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+const light = new THREE.PointLight(0xffffff, 2, 100);
+const light2 = new THREE.PointLight(0xffffff, 1, 100);
+
 
 const points = [];
 points.push(new THREE.Vector3(-10, 0, 0));
@@ -19,8 +24,8 @@ const geometryLine = new THREE.BufferGeometry().setFromPoints(points);
 
 
 //Materials
-const material = new THREE.MeshBasicMaterial({color:0x00ff00});
-const lineMaterial = new THREE.LineBasicMaterial({color: 0x0000ff});
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
 
 //Meshes
@@ -29,20 +34,33 @@ const line = new THREE.Line(geometryLine, lineMaterial);
 
 
 //Scene
-scene.add(cube);
-scene.add(line);
+scene.add(light);
+scene.add(light2);
+
+light.position.set(10, 10, 10);
+light2.position.set(-10, -10, -10);
+
+const loader = new GLTFLoader();
+
+loader.load('resources/rosa.glb', function(gltf) {
+
+    scene.add(gltf.scene);
+}, undefined, function(error) {
+    console.error(error);
+});
 
 
-camera.position.set(0, 0, 30)
+
+
+camera.position.set(0, 10, 15)
 camera.lookAt(0, 0, 0);
 
-function animate (){
+function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    
+
+
 }
 
 animate();
